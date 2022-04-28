bc0bf4a3dce28a2585df60f086b14984121ff06e
gridandy <andreygridov@gmail.com>
2022-04-28 21:31:17 +0300

    Update shodan_search.sh

--- shodan_search.sh
+++ shodan_search.sh
@@ -2,10 +2,10 @@
 #shodan init 
 #echo "Пошук........" 
 echo "Пошук по параметрах \"$1\""
-shodan search --limit 1000 $1 | awk {'print $1'} | awk '!seen[$0]++' > /home/kali/Downloads/prerelease/ranges.txt
-shodan search --limit 1000 $1 | awk {'print $2'} | awk '!seen[$0]++' > /home/kali/Downloads/prerelease/ports.txt
-wc -w /home/kali/Downloads/prerelease/ranges.txt | awk {'print "Знайдено -- "$1" -- IP адрес"'}
-CIP=$( wc -w /home/kali/Downloads/prerelease/ranges.txt | awk {'print $1'} )
+shodan search --limit 1000 $1 | awk {'print $1'} | awk '!seen[$0]++' > ranges.txt
+shodan search --limit 1000 $1 | awk {'print $2'} | awk '!seen[$0]++' > ports.txt
+wc -w ranges.txt | awk {'print "Знайдено -- "$1" -- IP адрес"'}
+CIP=$( wc -w ranges.txt | awk {'print $1'} )
 echo $CIP
 if [ $CIP -gt 0 ]
 then
