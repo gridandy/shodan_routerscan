99eeb3d5fd9be48244d7ee7499b3c82fd68e4a86
gridandy <andreygridov@gmail.com>
2022-04-28 21:27:20 +0300

    Update shodan_search.sh

--- shodan_search.sh
+++ shodan_search.sh
@@ -4,7 +4,7 @@
 echo "Пошук по параметрах \"$1\""
 shodan search --limit 1000 $1 | awk {'print $1'} | awk '!seen[$0]++' > /home/kali/Downloads/prerelease/ranges.txt
 shodan search --limit 1000 $1 | awk {'print $2'} | awk '!seen[$0]++' > /home/kali/Downloads/prerelease/ports.txt
-wc -w /home/kali/Downloads/prerelease/ranges.txt | awk {'print "Найдено -- "$1" -- IP адрес"'}
+wc -w /home/kali/Downloads/prerelease/ranges.txt | awk {'print "Знайдено -- "$1" -- IP адрес"'}
 CIP=$( wc -w /home/kali/Downloads/prerelease/ranges.txt | awk {'print $1'} )
 echo $CIP
 if [ $CIP -gt 0 ]
@@ -14,7 +14,3 @@ then
 else
 	echo "Вихiд"
 fi
-#echo "Найдено $1 IP адресс"
-#echo "Запуск  RS....."
-#wine RouterScan.exe
-
