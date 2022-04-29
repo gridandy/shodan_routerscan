ccf9ae68af67c7e423e6d3e214e6a6882b6a03de
gridandy <andreygridov@gmail.com>
2022-04-29 11:23:20 +0300

    Update shodan_search.sh

--- shodan_search.sh
+++ shodan_search.sh
@@ -6,7 +6,7 @@ shodan search --limit 1000 $1 | awk {'print $1'} | awk '!seen[$0]++' > ranges.tx
 shodan search --limit 1000 $1 | awk {'print $2'} | awk '!seen[$0]++' > ports.txt
 wc -w ranges.txt | awk {'print "Знайдено -- "$1" -- IP адрес"'}
 CIP=$( wc -w ranges.txt | awk {'print $1'} )
-echo $CIP
+#echo $CIP
 if [ $CIP -gt 0 ]
 then
 	echo "Запуск  RS....."
